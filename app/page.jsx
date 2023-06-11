3869acd2d5b9e7f030069f5979f97b89cd6fe7fb
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-06-11 18:17:12 -0300

    Creating form and the create prompt route.

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,7 @@ const Home = () => {
     <>
       <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-          Discover & Share
-          <br className="max-md:hidden" />
+          Discover & Share <br className="max-md:hidden" />
           <span className="orange_gradient text-center">
             AI-Powered Prompts
           </span>
